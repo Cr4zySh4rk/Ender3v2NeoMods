fecb4757a27ca1d9d887b28646fb3ea2535015f9
Adithya Manjunath <adithya@Adithyas-MacBook-Air.local>
2023-08-13 17:59:30 +0530

    Add OctoPrint backup and update led.py

--- OctoPrintConfig/led.py
+++ OctoPrintConfig/led.py
@@ -8,11 +8,13 @@ LED_PIN = 12          # GPIO pin connected to the pixels (18 uses PWM!).
 LED_FREQ_HZ = 800000  # LED signal frequency in hertz (usually 800khz)
 LED_DMA = 10          # DMA channel to use for generating signal (try 10)
 LED_BRIGHTNESS = 255  # Set to 0 for darkest and 255 for brightest
-LED_INVERT = False    # True to invert the signal (when using NPN transistor level shift)
+LED_INVERT = False    # True to invert the signal (when using NPN 
+transistor level shift)
 LED_CHANNEL = 0       # 0 or 1 (0 is recommended for Raspberry Pi 3)
 
 # Create PixelStrip object with appropriate configuration.
-strip = PixelStrip(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
+strip = PixelStrip(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, 
+LED_BRIGHTNESS, LED_CHANNEL)
 # Intialize the library (must be called once before other functions).
 strip.begin()
 
@@ -24,7 +26,10 @@ def color_wipe(color):
 
 def main():
     try:
-        color_wipe(Color(255, 255, 0)) # set color
+        strip.setPixelColor(0, Color(155, 0, 255))
+        strip.setPixelColor(1, Color(255, 255, 255))
+        strip.setPixelColor(2, Color(255, 255, 255))
+        strip.show()
     except:
         color_wipe(Color(0, 0, 0)) # off
 
